37e7d1e0089df261a1ee7116c3743604bd068e07
wushuai <659-4gpe145ln@dingtalk.com>
2024-03-25 15:05:40 +0800

    【部分内容优化】

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // 2 3 6 4 5 1
 
+        System.out.println("先开始背哪个" + new Random().nextInt(7));
     }
 }
