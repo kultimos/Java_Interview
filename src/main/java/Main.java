2861134110b1c344ca904839f17f82e223bd0adf
wushuai <t-wushuai@megvii.com>
2024-08-05 20:44:50 +0800

    【世纪魅族面试over_面试题更新】

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,31 +14,7 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        BiT biT1 = new BiT();
-        biT1.setNum(5);
-        BiT biT2 = new BiT();
-        BiT biT3 = new BiT();
-        biT2.setNum(100);
-        biT3.setNum(99);
-        List<BiT> list = Arrays.asList(biT1, biT2, biT3);
-        for(BiT u: list) {
-            System.out.println(u.getNum());
-        }
-        Collections.sort(list, (o1, o2) -> (o1.getNum()+2 - o2.getNum()));
-        for(BiT u: list) {
-            System.out.println(u.getNum());
-        }
-    }
-}
-
-class BiT {
-    private Integer num;
-
-    public Integer getNum() {
-        return num;
-    }
-
-    public void setNum(Integer num) {
-        this.num = num;
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("1",2);
     }
 }
